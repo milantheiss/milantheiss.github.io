51f983e0d8431fc7225d9b882ecc15747e43fcad
Milan Theiß <milan.theiss@gmx.net>
2022-12-09 14:48:02 +0100

    URL Update

--- ev3-scratch-interface-extension.js
+++ ev3-scratch-interface-extension.js
@@ -45,7 +45,7 @@ class ScratchFetch {
         };
     }
 
-    url = "https://e13d-213-157-8-90.eu.ngrok.io/"
+    url = "t.ly/ev3-scratch-interface-extension"
 
     forwards({timeout}){
         return fetch([this.url, "forwards?timeout=", timeout, "&speed=50"].join(""), {
